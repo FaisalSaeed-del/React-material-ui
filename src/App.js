4223005b343d6f264ece5a60b5387fba839f9ec1
FaisalSaeed-del <shoaibchudhary678@gmail.com>
2022-10-16 11:58:58 +0500

    message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/nav-bar/Navbar";
 import Hero from "./components/hero/Hero";
+import Form from "./components/form/Form";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Form />
     </div>
   );
 }

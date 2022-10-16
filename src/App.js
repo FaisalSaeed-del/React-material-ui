c6e2824fd75acab112678e7d1233629f9cd393b5
FaisalSaeed-del <shoaibchudhary678@gmail.com>
2022-10-16 12:07:39 +0500

    message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/nav-bar/Navbar";
 import Hero from "./components/hero/Hero";
 import Form from "./components/form/Form";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Form />
+      <Footer />
     </div>
   );
 }
